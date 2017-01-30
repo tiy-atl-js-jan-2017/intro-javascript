a430ab15912db4356d806a2238126e8775c37c55
Brit Butler <redline6561@gmail.com>
2017-01-30 12:15:18 -0500

    Get a fizzbuzz function and wire it to the button.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 console.log("Hello world");
 
-
 // Declare and initialize a variable
 var aGoodName = "Britton Stanhope Butler";
 
@@ -23,4 +22,16 @@ console.log(add(4, 5));
 var z = add(13, 17) + 8 + 5;
 
 // Don't do this very often.
-var crazy = add(add(5, 8), 42);
+// var crazy = add(add(5, 8), 42);
+
+
+
+var resultsDiv = document.getElementById("results");
+var textBox = document.getElementById("fizz-num");
+var button = document.getElementById("submit-button");
+
+function fizzBuzz () {
+  console.log("Wooo fiizzzbuzz");
+}
+
+button.addEventListener('click', fizzBuzz);
