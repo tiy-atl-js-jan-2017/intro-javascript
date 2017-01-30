d3d5527db8dfed405fcde48c2d6f93b2661b60f9
Brit Butler <redline6561@gmail.com>
2017-01-30 12:10:21 -0500

    More javascript examples.

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log("Hello world");
+
+
+// Declare and initialize a variable
+var aGoodName = "Britton Stanhope Butler";
+
+var weather = "raining";
+if (weather === "raining") {
+  console.log("order jjs");
+} else if (weather === "snowing") {
+  console.log("go the hell home");
+} else {
+  console.log("let's hit broad street");
+}
+
+// Function Definition
+function add (x, y) {
+  return x + y;
+}
+
+// Two examples of a Function Call
+console.log(add(4, 5));
+var z = add(13, 17) + 8 + 5;
+
+// Don't do this very often.
+var crazy = add(add(5, 8), 42);
